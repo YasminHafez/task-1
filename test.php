6cfb26d11804d9c91f2cb482a159ebe703c0603f
YasminHafez <yasmin33367@gmail.com>
2022-02-06 19:02:54 +0200

    'first'

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+
+$unit = 51;
+$price1 = 3.5;
+$price2 = 4;
+$price3 = 6;
+if($unit <= 50){
+    echo "the price is " .  $unit*$price1 . "$";
+}
+if($unit > 50 && $unit < 150) {
+    $calc = $unit * $price2;
+    echo "the price is {$calc}$";
+}
+if($unit >= 150){
+    echo "the price is " . $unit*$price3 . "$";
+}
+
+?>
